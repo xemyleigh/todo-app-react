029461a476985f8bd8a31f7ff0ffd28547f906b9
alexander <emyleigh@inbox.ru>
2022-10-19 20:48:11 +0300

    add Info page component

---
+++ src/components/Info.jsx
@@ -0,0 +1,19 @@
+const Info = () => {
+    return <div className="container m-5 d-flex justify-content-center">
+        <div className="col-10">
+            <h1>App Info</h1>
+            <p>This app allows you easily add lists and tasks to keep them on the screen.</p>
+            <p>There are to forms to use the Rememberer: </p>
+            <ul>
+                <li>Form to add lists</li>
+                <li>Form to add tasks</li>
+            </ul>
+            <p>Add your business inside to save it. In addition to this, inside tasks form you can add some description to your tasks.</p>
+            <p>To make your daily business management easier, there is an opportunity to mark tasks as 'done' and delete them from the list.</p>
+            <p>You can make different lists and switch between them. General list shows you all the tasks you had added.</p>
+        </div>
+
+        </div>
+}
+
+export default Info
