c0ea04fe3535cf88ebee9c6f08233aeace7c665d
alexander <emyleigh@inbox.ru>
2022-10-20 03:17:48 +0300

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/dist/collapse'
 import React from 'react';
-import TasksForm from './components/TasksForm';
-import TodoList from './components/TodoList'
-import ListsForm from './components/ListsForm'
-import ListsList from './components/ListsList';
-import Header from './components/Header'
+import MainPage from './components/MainPage';
+import Layout from './components/Layout'
 import { useSelector } from 'react-redux';
+import Heading from './components/Heading';
+import { Routes, Route } from 'react-router-dom'
+import {
+    BrowserRouter as Router
+  } from 'react-router-dom';
+import Info from './components/Info';
 
 
-function App() {
+const App = () => {
     const tasks = useSelector(state => Object.values(state.tasks.entities))
     console.log(tasks)
 
   return (
-    <div className="container text-dark mt-5">
-        <Header />
-        <div className="row">
-            <div className="col-lg-5 col-md-12 px-4 px-md-2 mb-3">
-                <div className='bg-light shadow bg-gradient rounded p-4'>
-                    <h1><b>Lists</b></h1>
-                    <ListsForm />
-                </div>
-            </div>
-            <div className="col-lg-7 col-md-12 px-4 px-md-2 mb-3">
-                <div className='bg-info shadow bg-gradient rounded p-4'>
-                    <h1><b>Tasks</b></h1>
-                    <TasksForm />
-                </div>
-            </div>  
-        </div>
-        <div className="row">
-            <ListsList />
-            <TodoList />
-        </div>
-    </div>
+    <Router>
+        <Routes>
+            <Route path='/' element={<Layout />} >
+                <Route index element={<MainPage />} />
+                <Route path='info' element={<Info />} />
+            </Route>
+        </Routes>
+    </Router>
   );
 }
 
